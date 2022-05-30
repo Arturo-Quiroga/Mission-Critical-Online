ab4cf4973179e964eca9d4438462646264814cce
Heyko Oelrichs <heoelri@microsoft.com>
2022-05-30 14:29:53 +0200

    drop old assignments

--- src/infra/monitoring/grafana/terraform/stamps/roleassignments.tf
+++ src/infra/monitoring/grafana/terraform/stamps/roleassignments.tf
@@ -1,18 +1,18 @@
 # Permission for App Services to pull images from the globally shared ACR
-resource "azurerm_role_assignment" "acrpull_role" {
-  for_each             = var.stamps
-  scope                = var.acr_resource_id
-  role_definition_name = "AcrPull"
-  principal_id         = azurerm_linux_web_app.appservice[each.key].identity[0].principal_id
-}
+#resource "azurerm_role_assignment" "acrpull_role" {
+#  for_each             = var.stamps
+#  scope                = var.acr_resource_id
+#  role_definition_name = "AcrPull"
+#  principal_id         = azurerm_linux_web_app.appservice[each.key].identity[0].principal_id
+#}
 
 # Permission for Grafana to read from all Log Analytics workspaces in the subscription
-resource "azurerm_role_assignment" "loganalyticsreader_role" {
-  for_each             = var.stamps
-  scope                = data.azurerm_subscription.current.id
-  role_definition_name = "Log Analytics Reader"
-  principal_id         = azurerm_linux_web_app.appservice[each.key].identity[0].principal_id
-}
+#resource "azurerm_role_assignment" "loganalyticsreader_role" {
+#  for_each             = var.stamps
+#  scope                = data.azurerm_subscription.current.id
+#  role_definition_name = "Log Analytics Reader"
+#  principal_id         = azurerm_linux_web_app.appservice[each.key].identity[0].principal_id
+#}
 
 # user assigned managed identity
 resource "azurerm_user_assigned_identity" "loganalytics_reader_mi" {
