3be348104b0bd851322c2f9377eb8a35fa23887d
Heyko Oelrichs <heoelri@microsoft.com>
2022-05-30 12:53:50 +0200

    change container image

--- src/infra/monitoring/grafana/terraform/stamps/variables.tf
+++ src/infra/monitoring/grafana/terraform/stamps/variables.tf
@@ -49,7 +49,7 @@ variable "db_admin_user" {
 variable "wapp_container_image" {
   description = "Docker image to use for Grafana."
   type        = string
-  default     = "heoelri/grafana:v3"
+  default     = "grafana/grafana:latest"
 }
 
 variable "frontdoor_header_id" {
